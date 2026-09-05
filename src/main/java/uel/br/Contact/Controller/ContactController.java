4eb921bafaf68388a28b408904387cb911c2b2cf
Igor <igor.oliveiraduarte05@gmail.com>
2026-09-05 16:37:27 -0300

    Adicionei a parte do controller mas sei que tem coisas erradas, ele só retorna status de criado sem ser exception

--- src/main/java/uel/br/Contact/Controller/ContactController.java
+++ src/main/java/uel/br/Contact/Controller/ContactController.java
@@ -5,6 +5,7 @@ import uel.br.Contact.Service.ContactService;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
+import
 import java.util.List;
 @RestController
 @RequestMapping("/contatos")
@@ -16,27 +17,32 @@ public class ContactController {
     }
 
     @GetMapping
+    @ResponseStatus(HttpStatus.OK)
     public List<ContactModel> listar(){
         return service.listar();
     }
 
     @GetMapping("/{index}")
+    @ResponseStatus(HttpStatus.OK)
     public ContactModel buscar(@PathVariable int index){
         return service.buscar(index);
     }
 
     @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
     public ContactModel adicionar(@RequestBody ContactModel contato) {
         return service.adicionar(contato);
     }
 
     @DeleteMapping("/{index}")
+    @ResponseStatus(HttpStatus.GONE)
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void remover(@PathVariable int index) {
         service.remover(index);
     }
 
     @PutMapping("/{index}")
+    @ResponseStatus(HttpStatus.OK)
     public void atualizar(@PathVariable int index, @RequestBody ContactModel contactModel) {
         service.atualizar(index, contactModel);
     }
