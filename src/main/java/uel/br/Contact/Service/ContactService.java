6e3c68701ace5f0324adf61a2fa5f08a17465c9c
Igor <igor.oliveiraduarte05@gmail.com>
2026-09-05 16:33:56 -0300

    Adicionando o primeiro parametro dentro de service, ainda achei meio estranho. Ele valida pelo indice ou pe;los Dados. Caso não tenha indice = Exception. Caso falte alguma informação = Exception.

--- src/main/java/uel/br/Contact/Service/ContactService.java
+++ src/main/java/uel/br/Contact/Service/ContactService.java
@@ -1,10 +1,11 @@
 package uel.br.Contact.Service;
 
+import org.apache.coyote.Response;
+import org.springframework.web.server.ResponseStatusException;
 import uel.br.Contact.Model.ContactModel;
 import org.springframework.stereotype.Service;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.IllegalFormatException;
 
 @Service
 public class ContactService {
@@ -15,21 +16,38 @@ public class ContactService {
     }
 
     public ContactModel buscar(int index){
+        validarIndice(index);
         return contatos.get(index);
     }
 
     public ContactModel adicionar(ContactModel c){
+        validarDados(c);
         contatos.add(c);
         return c;
     }
 
     public void remover(int index){
+        validarIndice(index);
         contatos.remove(index);
     }
 
     public ContactModel atualizar(int index, ContactModel c){
+        validarIndice(index);
+        validarDados(c);
         contatos.set(index, c);
         return c;
+    }
+
+
+    //400
+    private void validarDados(ContactModel c){
+        if(c.getNome()==null) throw new ResponseStatusException("O campo 'nome' não pode estar vazio.");
+        if(c.getTelefone()==null) throw new ResponseStatusException("O campo 'telefone' não pode estar vazio.");
+        if(c.getEmail()==null) throw  new ResponseStatusException("O campo 'email' não pode ficar vazio.");
+        if(c.getDataNascimento()==null) throw new ResponseStatusException("O campo 'data de nascimento' não pode ficar vazio.");
+    }
+    private void validarIndice(int index){
+        if(index < 0 || index >= contatos.size()) throw new IndexOutOfBoundsException("Contato não encontrado");
     }
 }
 
@@ -45,7 +63,7 @@ public class ContactService {
 
 //Adicionando Códigos de status IllegalArgumentException
 //Códigos de status a serem incorporados:
-//• 404 NOT FOUND: quando o contato não existir.
+//• 404 NOT FOUND: quando o contato não existir. (Validar Index)
 //• 400 BAD REQUEST: quando os dados enviados forem inválidos, isto é, nome, telefone ou e-mail nulos.
 //• 201 CREATED: ao adicionar um novo contato.
 //• 204 NO CONTENT: ao remover um contato.
